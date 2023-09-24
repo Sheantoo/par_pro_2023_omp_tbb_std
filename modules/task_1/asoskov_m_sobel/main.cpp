62f6313638d6946a5147feb88e5af72c3786d60a
MaksaLegend <61541647+Sheantoo@users.noreply.github.com>
2023-09-24 22:12:53 +0300

    sobel_seq

---
+++ modules/task_1/asoskov_m_sobel/main.cpp
@@ -0,0 +1,38 @@
+// Copyright 2022 Asoskov Michael
+#include "./sobel.h"
+#include "gtest/gtest.h"
+
+TEST(TEST_SOBOL, SIZE_MATR_100) {
+  Sobel s1;
+  size_t size = 100;
+  ASSERT_NO_FATAL_FAILURE(s1.operator_Sobel(s1.RandomMatrix(size)));
+}
+
+TEST(TEST_SOBOL, SIZE_MATR_500) {
+  Sobel s1;
+  size_t size = 500;
+  ASSERT_NO_FATAL_FAILURE(s1.operator_Sobel(s1.RandomMatrix(size)));
+}
+
+TEST(TEST_SOBOL, SIZE_MATR_1000) {
+  Sobel s1;
+  size_t size = 1000;
+  ASSERT_NO_FATAL_FAILURE(s1.operator_Sobel(s1.RandomMatrix(size)));
+}
+
+TEST(TEST_SOBOL, SIZE_MATR_2000) {
+  Sobel s1;
+  size_t size = 2000;
+  ASSERT_NO_FATAL_FAILURE(s1.operator_Sobel(s1.RandomMatrix(size)));
+}
+
+TEST(TEST_SOBOL, SIZE_MATR_3000) {
+  Sobel s1;
+  size_t size = 3000;
+  ASSERT_NO_FATAL_FAILURE(s1.operator_Sobel(s1.RandomMatrix(size)));
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
